3e37a6cadad3f081a1ab72936a0f6d645e66a116
Brian Nguyen <BrianCTNguyen@knights.ucf.edu>
2021-06-06 20:44:00 -0400

    Initial commit

---
+++ src/main/java/base/app.java
@@ -0,0 +1,39 @@
+package base;
+
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Brian Nguyen
+ */
+
+/*
+Create a program that prompts for your name and prints a greeting using your name.
+
+Example Output
+
+What is your name? Brian
+Hello, Brian, nice to meet you!
+
+Constraint
+
+Keep the input, string concatenation, and output separate.
+
+Challenges
+
+Write a new version of the program without using any variables.
+Write a version of the program that displays different greetings for different people.
+Write a version of the program that displays the greeting in a GUI window.
+ */
+
+public class app {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is you name? ");
+        String name = in.nextLine();
+
+        String outputString = "Hello, " + name + ", nice to meet you!";
+
+        System.out.println(outputString);
+    }
+}
